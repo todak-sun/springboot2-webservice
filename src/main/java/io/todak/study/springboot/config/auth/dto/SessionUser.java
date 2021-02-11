c37c499535bbb6f37d0635b210c92b1220de05f0
todak <tjsdydwn@gmail.com>
2021-02-11 14:08:10 +0900

    스프링 시큐리티 적용 및 구글로그인 적용

---
+++ src/main/java/io/todak/study/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,20 @@
+package io.todak.study.springboot.config.auth.dto;
+
+import io.todak.study.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
